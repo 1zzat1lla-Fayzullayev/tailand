88b49ef9f36333eb87dc6a7e3d0e67cab4f6f7f8
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-07-12 17:18:13 +0500

    remove

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,7 +1,6 @@
 const Map = () => {
   return (
     <div className="mt-[80px]">
-      {/* Mobile image */}
       <div className="block md:hidden w-full h-[300px] relative">
         <img
           src="/29.jpg"
@@ -10,7 +9,6 @@ const Map = () => {
         />
       </div>
 
-      {/* Background for md+ screens */}
       <div
         className="hidden md:block texts pt-10 md:pt-[60px] lg:pt-[80px] xl:pt-[100px] 2xl:pt-[130px] pl-10 md:pl-[60px] lg:pl-[80px] xl:pl-[100px] 2xl:pl-[130px] w-full min-h-screen"
         style={{
